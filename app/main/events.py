58d106a0071ee37ab4ceae33f3598fe2b8f9fc32
Burak Uyar <burakuyar25@gmail.com>
2018-03-02 08:10:19 +0300

    Dont get your own message from server.

--- app/main/events.py
+++ app/main/events.py
@@ -24,7 +24,7 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room, broadcast=True, include_self=False)
 
 
 @socketio.on('left', namespace='/chat')
