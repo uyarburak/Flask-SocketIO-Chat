903fadc677d3468a98959f4e2c1904b8d616438f
Burak Uyar <burakuyar25@gmail.com>
2018-03-07 21:21:49 +0300

    Password field is added to login form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
-from wtforms.fields import StringField, SubmitField
+from wtforms.fields import StringField, SubmitField, PasswordField
 from wtforms.validators import Required
 
 
 class LoginForm(Form):
-    """Accepts a nickname and a room."""
+    """Accepts a nickname and a password."""
     name = StringField('Name', validators=[Required()])
+    password = PasswordField('Password', validators=[Required()])
     room = StringField('Room', validators=[Required()])
     submit = SubmitField('Enter Chatroom')
