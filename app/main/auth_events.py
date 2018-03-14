79077493718308f6bb9b876f160eb766e6e75ebf
Burak Uyar <burakuyar25@gmail.com>
2018-03-14 19:37:26 +0300

    Changed SHA1 to SHA256 and second step of CR Auth protocol has been implemented.

--- app/main/auth_events.py
+++ app/main/auth_events.py
@@ -5,23 +5,24 @@ from flask_socketio import emit
 from .. import socketio
 from .users import getUser
 from .kripto import decryptRSA, encryptAES, decryptAES
-from hashlib import sha1
+from hashlib import sha256
 import string
 import random
 import hmac
+import json
 
-def hmac_sha1(secret_key, string):
+def hmac_sha256(secret_key, string):
     string_to_sign = string.encode('utf-8')
     key_to_sign = secret_key.encode('utf-8')
-    hashed = hmac.new(key_to_sign,string_to_sign, sha1)
+    hashed = hmac.new(key_to_sign,string_to_sign, sha256)
     return hashed.hexdigest()
 
 def hex_sha1(string):
     string_to_sign = string.encode('utf-8')
-    hashed = sha1(string_to_sign)
+    hashed = sha256(string_to_sign)
     return hashed.hexdigest()
 
-def generateRandomChallenge(length=120):
+def generateRandomChallenge(length=15):
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
 
 @socketio.on('start_protocol', namespace='/auth')
@@ -32,3 +33,12 @@ def start_protocol(message):
     ciphertext = encryptAES(user['password'], random_challenge)
     print ("ciphertext: ", ciphertext)
     emit('protocol_level_1', {'challenge': ciphertext}, include_self=True, Broadcast=False)
+
+@socketio.on('proof_of_id', namespace='/auth')
+def start_protocol(message):
+    user = getUser(message['name'])
+    ciphertext = message['ciphertext']
+    plaintext = decryptRSA(ciphertext)
+    obj = json.loads(plaintext)
+    print ("given challenge: ", obj['challenge'])
+    print ("given session_key: ", obj['session_key'])
