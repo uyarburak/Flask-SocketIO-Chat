a5ee9b4e7c4d3975217353ec91a42d145a734b99
Burak Uyar <burakuyar25@gmail.com>
2018-03-14 17:05:47 +0300

    Cryptology class has been added.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import routes, events, users, auth_events
+from . import routes, events, users, auth_events, kripto
 
 users.initForDemo()
+kripto.initKripto()
